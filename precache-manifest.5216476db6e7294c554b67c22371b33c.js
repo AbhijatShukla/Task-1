0d41c55a6dcb74c703d47abeac7a1d32151a8f89
Abhijat Shukla <17531901+AbhijatShukla@users.noreply.github.com>
2022-04-02 18:17:35 +0530

    testbuild

---
+++ precache-manifest.5216476db6e7294c554b67c22371b33c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "27689ea70951dc3db9ed",
+    "url": "/AbhijatShukla/Task-1/static/css/main.01415545.chunk.css"
+  },
+  {
+    "revision": "27689ea70951dc3db9ed",
+    "url": "/AbhijatShukla/Task-1/static/js/main.27689ea7.chunk.js"
+  },
+  {
+    "revision": "2998caf54286a93ccb62",
+    "url": "/AbhijatShukla/Task-1/static/js/1.2998caf5.chunk.js"
+  },
+  {
+    "revision": "74a02f9620d565895c1d",
+    "url": "/AbhijatShukla/Task-1/static/js/2.74a02f96.chunk.js"
+  },
+  {
+    "revision": "c5aa8fcf2452054b9064",
+    "url": "/AbhijatShukla/Task-1/static/js/runtime~main.c5aa8fcf.js"
+  },
+  {
+    "revision": "e956cdfbd9d89eafd93942f5e1be0847",
+    "url": "/AbhijatShukla/Task-1/index.html"
+  }
+];
